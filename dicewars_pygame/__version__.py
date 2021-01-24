3bb8dee2dd5b0d3ade9ee19082e6a4b4c87a50e9
Scotty <scotty@c-base.org>
2021-01-24 13:10:06 +0100

    Add basic package and license files

---
+++ dicewars_pygame/__version__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2021 Thomas Schott <scotty@c-base.org>
+#
+# This file is part of dicewars_pygame.
+#
+# dicewars_pygame is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# dicewars_pygame is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with dicewars_pygame.  If not, see <http://www.gnu.org/licenses/>.
+
+VERSION = (0, 1, 0, 'dev0')
+__version__ = '.'.join(str(v) for v in VERSION)
